cea5fe85a1b073943ec5a528b1e0769aa5b7cd93
Lonnie James <lj558231@gmail.com>
2024-01-03 14:29:42 +0100

    FORM : confirmation message is now displayed if the form is submitted

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -16,6 +16,7 @@ const Form = ({ onSuccess, onError }) => {
       try {
         await mockContactApi();
         setSending(false);
+        onSuccess(); // Ajout de la fonction onSuccess() à la soumission du formulaire //
       } catch (err) {
         setSending(false);
         onError(err);
